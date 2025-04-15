2b96a7404a63ec92995f1f399c284d0cc992d0e1
Bobby Flennoy <87404756+BobbyAl@users.noreply.github.com>
2025-04-15 03:41:35 -0500

    Fix basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/encrypt-site',
+  basePath: '/crypt-webapp',
   images: {
     unoptimized: true,
   },
